d0a384a0b0949ad2395ba8807a7dfd5247d9c05e
PROBOOK 445R G7 <desmondkudjuh99@gmail.com>
2025-03-16 17:44:16 +0000

    update user

--- src/app/api/auth/mobile/otp-generation/route.ts
+++ src/app/api/auth/mobile/otp-generation/route.ts
@@ -6,7 +6,7 @@ import { z } from "zod";
 
 const verificationSchema = z.object({
   identifier: z.string().min(3),
-  route: z.string().optional().nullable(),
+  route: z.string().optional().nullish(),
 });
 
 export const POST = async (request: Request) => {
